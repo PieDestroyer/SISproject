17e447a7f203a4d9f2dff1ea8c2846e10104ccb6
ryan.manning <ryan.manning@MU-C3-06.mullen.pvt.k12.co.us>
2014-10-20 08:56:05 -0600

    getters & setters

--- SISproject/src/Constructors.java
+++ SISproject/src/Constructors.java
@@ -16,10 +16,11 @@ public class Constructors
 	private int Period3 = 3;
 	private String class3;
 	private String gradeClass3;
+	private double GPA = 0;
 	
 	static ArrayList <Constructors> database = new ArrayList <Constructors> ();
 	
-public Constructors(String f, String l, int p1, String c1, String g1, int p2, String c2, String g2, int p3, String c3, String g3)
+public Constructors(String f, String l, int p1, String c1, String g1, int p2, String c2, String g2, int p3, String c3, String g3, double GpA)
 {
 	firstName = f;
 	lastName = l; 
@@ -32,6 +33,7 @@ public Constructors(String f, String l, int p1, String c1, String g1, int p2, St
 	Period3 = p3;
 	class3 = c3;
 	gradeClass3 = g3;
+	GPA = GpA; 
 }
 
 public void createArraylist() throws FileNotFoundException
@@ -52,9 +54,115 @@ while(file.hasNext())
 	class3 = file.next();
 	gradeClass3 = file.next();
 	
-	database.add(new Constructors(firstName, lastName, Period1, class1, gradeClass1, Period2, class2, gradeClass2, Period3, class3, gradeClass3));
+	database.add(new Constructors(firstName, lastName, Period1, class1, gradeClass1, Period2, class2, gradeClass2, Period3, class3, gradeClass3, GPA));
 	
 }
 
 	}
+
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public int getPeriod1() {
+	return Period1;
+}
+
+public void setPeriod1(int period1) {
+	Period1 = period1;
+}
+
+public String getClass1() {
+	return class1;
+}
+
+public void setClass1(String class1) {
+	this.class1 = class1;
+}
+
+public String getGradeClass1() {
+	return gradeClass1;
+}
+
+public void setGradeClass1(String gradeClass1) {
+	this.gradeClass1 = gradeClass1;
+}
+
+public int getPeriod2() {
+	return Period2;
+}
+
+public void setPeriod2(int period2) {
+	Period2 = period2;
+}
+
+public String getClass2() {
+	return class2;
+}
+
+public void setClass2(String class2) {
+	this.class2 = class2;
+}
+
+public String getGradeClass2() {
+	return gradeClass2;
+}
+
+public void setGradeClass2(String gradeClass2) {
+	this.gradeClass2 = gradeClass2;
+}
+
+public int getPeriod3() {
+	return Period3;
+}
+
+public void setPeriod3(int period3) {
+	Period3 = period3;
+}
+
+public String getClass3() {
+	return class3;
+}
+
+public void setClass3(String class3) {
+	this.class3 = class3;
+}
+
+public String getGradeClass3() {
+	return gradeClass3;
+}
+
+public void setGradeClass3(String gradeClass3) {
+	this.gradeClass3 = gradeClass3;
+}
+
+public double getGPA() {
+	return GPA;
+}
+
+public void setGPA(double gPA) {
+	GPA = gPA;
+}
+
+public static ArrayList<Constructors> getDatabase() {
+	return database;
+}
+
+public static void setDatabase(ArrayList<Constructors> database) {
+	Constructors.database = database;
+}
+
+
 }
