6e53deb54bcce0a37643389b9d631e10b177e14c
Anthony.Yarosh <Anthony.Yarosh@MU-C3-05.mullen.pvt.k12.co.us>
2014-10-21 08:44:54 -0600

    Finished Main

--- SISproject/src/Main.java
+++ SISproject/src/Main.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
 
 public class Main
 	{
-//mine
-	//commit from home
+	// mine
+	// commit from home
 	public static void main(String[] args)
 		{
-		
-		
-
+		System.out.println("What would you like to do?");
+		System.out.println();
+		System.out.println("<1> Add or delete a student");
+		System.out.println("<2> Change student grades or schedule");
+		System.out.println("<3> Sort students");
+		option = input.nextInt();
+		if (option == 1)
+			{
+			AddOrDeleteStudent.Intro();
+			}
+		if (option == 2)
+			{
+			
+			}
+		if (option == 3)
+			{
+			
+			}
 		}
 
 	}
