9a6951d30d7c71182c0fd7b99732df0ab41b9cd6
anthony.yarosh <anthony.yarosh@MU-C3-05.mullen.pvt.k12.co.us>
2014-10-22 10:10:14 -0600

    Finished main part, doing add/delete

--- SISproject/src/AddOrDeleteStudent.java
+++ SISproject/src/AddOrDeleteStudent.java
@@ -1,37 +1,53 @@
 import java.util.Scanner;
 
-
 public class AddOrDeleteStudent
-{
-public static void main(String[] args)
 	{
-	Intro();
-	AddStudent();
-	DeleteStudent();
-	}
 
-public static void Intro()
-	{
-	int option;
-	Scanner input = new Scanner(System.in);
-	System.out.println("<1> Add student");
-	System.out.println("<2> Delete student");
-	if (option == 1)
+	public static void main(String[] args)
 		{
+		Intro();
 		AddStudent();
+		DeleteStudent();
 		}
-	if (option == 2)
-		{
-		
-		}
-	}
 
-public static void AddStudent()
-	{
-	
+	public static void Intro()
+		{
+		int option;
+		Scanner input = new Scanner(System.in);
+		System.out.println("<1> Add student");
+		System.out.println("<2> Delete student");
+		option = input.nextInt();
+		if (option == 1)
+			{
+			AddStudent();
+			}
+		if (option == 2)
+			{
+
+			}
+		}
+
+	public static void AddStudent()
+		{
+		String firstName;
+		String lastName;
+		Scanner input = new Scanner(System.in);
+		System.out.println("First name?");
+		firstName = input.nextLine();
+		System.out.println("Last name?");
+		lastName = input.nextLine();
+		System.out.println("Class 1?");
+		lastName = input.nextLine();
+		System.out.println("Last name?");
+		lastName = input.nextLine();
+		System.out.println("Last name?");
+		lastName = input.nextLine();
+		
+
+		}
+
+	public static void DeleteStudent()
+		{
+
+		}
 	}
-public static void DeleteStudent()
-	{
-	
-	}
-}
