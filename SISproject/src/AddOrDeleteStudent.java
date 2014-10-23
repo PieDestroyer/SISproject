f5860b8a20fd9dcfb43579e34e546b85ba99ce5b
ryan.manning <ryan.manning@MU-C3-06.mullen.pvt.k12.co.us>
2014-10-23 08:08:19 -0600

    Finished Add

--- SISproject/src/AddOrDeleteStudent.java
+++ SISproject/src/AddOrDeleteStudent.java
@@ -30,37 +30,46 @@ public class AddOrDeleteStudent
 		}
 
 	public static void AddStudent() throws IOException
-		{
-		fillArray.createArraylist();
-		String firstName;
-		String lastName;
-		String class1;
-		String grade1;
-		String class2;
-		String grade2;
-		String class3;
-		String grade3;
-		
-		Scanner input = new Scanner(System.in);
-		System.out.println("First name?");
-		firstName = input.nextLine();
-		System.out.println("Last name?");
-		lastName = input.nextLine();
-		System.out.println("Class 1?");
-		class1 = input.nextLine();
-		System.out.println("Grade?");
-		grade1 = input.nextLine();
-		System.out.println("Class 2?");
-		class2 = input.nextLine();
-		System.out.println("Grade?");
-		grade2 = input.nextLine();
-		System.out.println("Class 3?");
-		class3 = input.nextLine();
-		System.out.println("Grade?");
-		grade3 = input.nextLine();
-		System.out.println(Students.database + "\n" + firstName + " " + lastName + " " + class1 + " " + grade1 + " " + class2 + " " + grade2 + " " + class3 + " " + grade3);
+	{
+	fillArray.createArraylist();
+	String firstName;
+	String lastName;
+	int Period1 = 1;
+	String class1;
+	String grade1;
+	int Period2 = 2;
+	String class2;
+	String grade2;
+	int Period3 = 3;
+	String class3;
+	String grade3;
+	double GPA = 0.0;
+	
+	Scanner input = new Scanner(System.in);
+	System.out.println("First name?");
+	firstName = input.nextLine();
+	System.out.println("Last name?");
+	lastName = input.nextLine();
+	System.out.println("Class 1?");
+	class1 = input.nextLine();
+	System.out.println("Grade?");
+	grade1 = input.nextLine();
+	System.out.println("Class 2?");
+	class2 = input.nextLine();
+	System.out.println("Grade?");
+	grade2 = input.nextLine();
+	System.out.println("Class 3?");
+	class3 = input.nextLine();
+	System.out.println("Grade?");
+	grade3 = input.nextLine();
+	Students.database.add(new Students(firstName, lastName,Period1,class1, grade1,Period2, class2, grade2, Period3, class3, grade3, GPA));
+	
+	makeGPA.createGPA();
+	System.out.println(Students.database);
+	//+ "\n" + firstName + " " + lastName + " " + Period1 + "" + class1 + " " + grade1 + " " + Period2 + " " + class2 + " " + grade2 + Period3 + " " + " " + class3 + " " + grade3);
 
-		}
+	}
+
 
 	public static void DeleteStudent()
 		{
