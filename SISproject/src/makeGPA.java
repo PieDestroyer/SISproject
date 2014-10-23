1221d560ba75e35e4e2ee8a510cde66fa1da7d65
Laptop <Laptop@laptop-6e614daf>
2014-10-22 21:00:34 -0600

    Make GPA program done trial 1

--- SISproject/src/makeGPA.java
+++ SISproject/src/makeGPA.java
@@ -2,4 +2,194 @@
 public class makeGPA
 	{
 
+public void createGPA()
+{
+	for (int i = 0; i < Students.database.size(); i++)
+	{
+		//set GPA from grade of class 1
+		switch(Students.database.get(i).getGradeClass1())
+		{
+		case "A+": 
+		{
+			Students.database.get(i).setGPA(4.3);	
+		}
+		case "A": 
+		{
+			Students.database.get(i).setGPA(4.0);
+		}
+		case "A-": 
+		{
+			Students.database.get(i).setGPA(3.7);
+		}
+		case "B+": 
+		{
+			Students.database.get(i).setGPA(3.3);
+		}
+		case "B": 
+		{
+			Students.database.get(i).setGPA(3.0);
+		}
+		case "B-": 
+		{
+			Students.database.get(i).setGPA(2.7);
+		}
+		case "C+": 
+		{
+			Students.database.get(i).setGPA(2.3);
+		}
+		case "C": 
+		{
+			Students.database.get(i).setGPA(2.0);
+		}
+		case "C-": 
+		{
+			Students.database.get(i).setGPA(1.7);
+		}
+		case "D+": 
+		{
+			Students.database.get(i).setGPA(1.3);
+		}
+		case "D": 
+		{
+			Students.database.get(i).setGPA(1.0);
+		}
+		case "D-": 
+		{
+			Students.database.get(i).setGPA(0.7);
+		}
+		case "F+": 
+		{
+			Students.database.get(i).setGPA(0.3);
+		}
+		case "F": 
+		{
+			Students.database.get(i).setGPA(0.0);
+		}
 	}
+	//set GPA from grade of class 2
+		switch(Students.database.get(i).getGradeClass2())
+		{
+		case "A+": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 4.3)/2);	
+		}
+		case "A": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 4.0)/2);
+		}
+		case "A-": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 3.7)/2);
+		}
+		case "B+": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 3.3)/2);
+		}
+		case "B": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 3.0)/2);
+		}
+		case "B-": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 2.7)/2);
+		}
+		case "C+": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 2.3)/2);
+		}
+		case "C": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 2.0)/2);
+		}
+		case "C-": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 1.7)/2);
+		}
+		case "D+": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 1.3)/2);
+		}
+		case "D": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 1.0)/2);
+		}
+		case "D-": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 0.7)/2);
+		}
+		case "F+": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 0.3)/2);
+		}
+		case "F": 
+		{
+			Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 0.0)/2);
+		}
+	}
+	
+		//set GPA from grade of class 3
+				switch(Students.database.get(i).getGradeClass3())
+				{
+				case "A+": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 4.3)/3);	
+				}
+				case "A": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 4.0)/3);
+				}
+				case "A-": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 3.7)/3);
+				}
+				case "B+": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 3.3)/3);
+				}
+				case "B": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 3.0)/3);
+				}
+				case "B-": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 2.7)/3);
+				}
+				case "C+": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 2.3)/3);
+				}
+				case "C": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 2.0)/3);
+				}
+				case "C-": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 1.7)/3);
+				}
+				case "D+": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 1.3)/3);
+				}
+				case "D": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 1.0)/3);
+				}
+				case "D-": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 0.7)/3);
+				}
+				case "F+": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 0.3)/3);
+				}
+				case "F": 
+				{
+					Students.database.get(i).setGPA((Students.database.get(i).getGPA() + 0.0)/3);
+				}
+			}
+			
+
+}
+}
+}
+
