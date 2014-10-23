8e7d211077cfbbc4877815b24ff0fd322bb8bb93
Cole.Donovan <Cole.Donovan@MU-C3-08.mullen.pvt.k12.co.us>
2014-10-23 09:09:23 -0600

    work

--- SISproject/src/ChangeGrade.java
+++ SISproject/src/ChangeGrade.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ChangeGrade
@@ -7,38 +8,21 @@ public class ChangeGrade
 	private static String lastNameCheck;
 	private static String gradeNew;
 
-	public static void changeGrade() {
-		System.out.println("What is the last name of the student whose grade you wish to change?");
-		lastNameCheck = read.next();
-		for (int i = 0; i < Students.database.size(); i++) 
+	public static void changeGrade() throws IOException
+		{
+		fillArray.createArraylist();
+		for(int x=0;x<Students.database.size(); x++)
 			{
-				if(Students.database.get(i).getLastName().equals(lastNameCheck)) 
-					{
-						System.out.println("What period's grade would you like to change. ex. 1, 2, or 3");
-						int periodChoice = read.nextInt();
-						System.out.println("What would you to change the grade of Mr. or Ms. "+ Students.database.get(i).getLastName() + " to");
-						String gradeNew = read.next();
-						if (periodChoice == 1) 
-							{
-								Students.database.get(i).setGradeClass1(gradeNew);
-							}
-						else if(periodChoice == 2)
-							{
-								Students.database.get(i).setGradeClass2(gradeNew);
-							}
-						else if(periodChoice == 3)
-							{
-								Students.database.get(i).setGradeClass3(gradeNew);
-							}
-						else
-							{
-								System.out.println("Sorry, that is not an option");
-							}
-					}
-				else
-					{
-					System.out.println("Sorry, that is not a valid last name.");
-					}
+			System.out.println((x+1)+".)"+" "+Students.database.get(x));
+			}
+		System.out.println("What is the number");
+		for(int i = 0; i < Students.database.size(); i++)
+			{
+			if (Students.database.get(i).getLastName().equals(lastNameCheck))
+				{
+				
+				}
+				
+			}
 		}
 	}
-}
